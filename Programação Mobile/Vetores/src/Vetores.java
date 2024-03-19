f18782dc12b44f56bf344298200c4d055fd1d527
KaiqueVfreitas <kaique.vieira2502Eu@outlook>
2024-03-19 14:12:30 -0300

    Começando a commitar todos os projetos que farei na etec, desde 19/03/2024 até a conclusão do curso, final de novembro de 2024

---
+++ Programação Mobile/Vetores/src/Vetores.java
@@ -0,0 +1,27 @@
+
+public class Vetores {
+
+	public Vetores() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ex1Nomes();
+	}
+
+	private static void ex1Nomes() {
+		// TODO Auto-generated method stub
+		String [] nomes = new String [5];
+		nomes [0] = "Ana";
+		nomes [1] = "João";
+		nomes [2] = "Rebecca";
+		nomes [3] = "Guilherme";
+		nomes [4] = "Kaique";
+		
+		while(nomes < nomes[4]) {
+			System.out.println(nomes);
+		}
+	}
+
+}
