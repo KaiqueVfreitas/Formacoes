99e06d6365200abc07fa928c9c8a48fbd1938e30
KaiqueVfreitas <kaique.vieira2502Eu@outlook>
2024-03-22 15:07:17 -0300

    Na aula de Java foi apenas maneiras de melhorar a logica

--- Programação Java/bgAdsAula04Laço/src/bgAdsAula04Laço/baseLaço.java
+++ Programação Java/bgAdsAula04Laço/src/bgAdsAula04Laço/baseLaço.java
@@ -6,7 +6,18 @@ public class baseLaço {
 	}
 
 	public static void main(String[] args) {
-		exTrianPascal();
+		exTabuadasResult();
+	}
+
+	private static void exTabuadasResult() {
+		// TODO Auto-generated method stub
+		for (int j = 1; j <= 10; j++) {
+			for (int i = 1; i <= 10; i++) {
+				System.out.println(i * j + "\t");
+
+			}
+			System.out.println();
+		}
 	}
 
 	private static void exTrianPascal() {
@@ -15,31 +26,31 @@ public class baseLaço {
 			for(int j=1; j<=i; j++){
 				System.out.print(i);
 			}
-		System.out.println();
+			System.out.println();
 		}
 	}
 
 	private static void exTesteMesaFor() {
 		// Os exercicios de mesa está comentado
-		
-		
+
+
 		//PRIMEIRO EXERCICIO
 		//int valor = 3;
 		//for (int i = 1; i <= 10; i++) {
-		   // System.out.println(i + "*" + valor + "=" + i * valor);
+		// System.out.println(i + "*" + valor + "=" + i * valor);
 		//}
-		
+
 		//SEGUNDO EXERCICIO
 		//for (int i=1; i <= 5; i++) {
-			//for (int j=1; j<=i; j++) {
-				//System.out.print(i);
-			//}
-			//System.out.println();
+		//for (int j=1; j<=i; j++) {
+		//System.out.print(i);
 		//}
-		
-		
+		//System.out.println();
+		//}
 
-		
+
+
+
 	}
 
 	private static void exFatorialWhile() {
@@ -50,7 +61,7 @@ public class baseLaço {
 			valor--;
 			System.out.println(fat);
 		}
-		
+
 	}
 
 	private static void exFatorialFor() {
@@ -59,7 +70,7 @@ public class baseLaço {
 			fat = fat*valor;
 			System.out.println(fat);
 		}
-		
+
 	}
 
 }
