09c1016648bc7d99769d601aa1962210cfa22c70
KaiqueVfreitas <kaique.vieira2502eu@outlook.com>
2024-05-06 15:52:02 -0300

    Estou desenvolvendo um aplicativo de avaliação de filmes nas aulas de programação mobile. E projetei 4 bancos de dados nas aulas de desenvolvimento

--- Projetos feitos na ETEC/2Semestre/Programacao Mobile/listaAvaliacaoFilmes/app/src/main/java/com/example/listaavaliacaofilmes/FilmeDao.java
+++ Projetos feitos na ETEC/2Semestre/Programacao Mobile/listaAvaliacaoFilmes/app/src/main/java/com/example/listaavaliacaofilmes/FilmeDao.java
@@ -2,8 +2,11 @@ package com.example.listaavaliacaofilmes;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+
+import java.util.ArrayList;
 
 public class FilmeDao extends SQLiteOpenHelper {
 
@@ -18,7 +21,6 @@ public class FilmeDao extends SQLiteOpenHelper {
                 "id integer primary key," + //por padrão já é autoincrement
                 "nome varchar(200) not null," +
                 "genero varchar(30) not null," +
-                "tipo char(5) not null, " +
                 "nota decimal(3,1) not null," +
                 "tipo char(5) not null)";
         db.execSQL(comando);
@@ -44,8 +46,22 @@ public class FilmeDao extends SQLiteOpenHelper {
         values.put("tipo", filme.tipo);
 
         getWritableDatabase().insertOrThrow("tb_filme", null, values);
+    }
 
+    public ArrayList<FilmeModel> consultarTodos(){
+        String comando = "select *  from tb_filme";
+        Cursor cursor = getReadableDatabase().rawQuery(comando,null);
+        ArrayList<FilmeModel> listFilmes = new ArrayList<>();
 
-
+        while (cursor.moveToNext()){
+            FilmeModel filmeModel = new FilmeModel();
+            filmeModel.id = cursor.getInt(0);
+            filmeModel.nome = cursor.getString(1);
+            filmeModel.genero = cursor.getString(2);
+            filmeModel.nota = cursor.getDouble(3);
+            filmeModel.tipo = cursor.getString(4);
+            listFilmes.add(filmeModel);
+        }
+        return listFilmes;
     }
 }
